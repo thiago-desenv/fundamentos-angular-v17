8f7c6b5940fa3f04f650d8c3680e8e41b1a2e805
thiago_osim <thiago_osim@hotmail.com>
2026-01-08 20:03:58 -0300

    Criado componente filho como exemplo

---
+++ src/app/filho/filho.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filho',
+  templateUrl: './filho.component.html',
+  styleUrl: './filho.component.scss'
+})
+export class FilhoComponent {
+
+}
