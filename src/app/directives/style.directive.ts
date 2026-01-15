05dc4c99abfdcb0e332c0e2a5ae7897af69aa7eb
thiago_osim <thiago_osim@hotmail.com>
2026-01-15 19:32:19 -0300

    Usado @HostBinding de diferentes formas para ilustrar sua implementação

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -4,5 +4,10 @@ import { Directive, HostBinding } from "@angular/core";
   selector: '[appStyle]'
 })
 export class StyleDirective {
-  @HostBinding('attr.style') attrStyle = 'background-color: orange;';
+  // @HostBinding('attr.style') attrStyle = 'background-color: orange;';
+  // @HostBinding('style') propStyle = 'background-color: red;'
+  // @HostBinding('style') propStyleObj = { backgroundColor: 'pink', color: 'white' };
+  // @HostBinding('style.backgroundColor') bgColor = 'green';
+  // @HostBinding('style.color') color = 'white';
+  @HostBinding('style.fontSize.px') size = 100;
 }
