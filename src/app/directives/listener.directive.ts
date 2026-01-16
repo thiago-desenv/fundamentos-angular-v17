ed731cde27889810a7c75ed931d85b3b5105939b
thiago_osim <thiago_osim@hotmail.com>
2026-01-15 22:49:35 -0300

    Usado @HostListener com eventos de mouse

--- src/app/directives/listener.directive.ts
+++ src/app/directives/listener.directive.ts
@@ -7,4 +7,11 @@ export class ListenerDirective {
   @HostListener('click') onClick() {
     console.log('Clicado');
   }
+
+  @HostListener('keyup', ['$event', '"Meu argumento"']) onKeyup(event: KeyboardEvent, param2: string) {
+    console.log('Digitou', event);
+    console.log('Param2', param2);
+    const valueTextInput = (event.target as HTMLInputElement).value;
+    console.log('Valor digitado', valueTextInput);
+  }
 }
