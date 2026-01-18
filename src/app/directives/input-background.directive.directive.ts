09b8dccf01f415770558ec3957df49ddb18cbc11
thiago_osim <thiago_osim@hotmail.com>
2026-01-18 19:29:53 -0300

    Usado hostbinding com um misto de eventos de mouse no HTML

--- src/app/directives/input-background.directive.directive.ts
+++ src/app/directives/input-background.directive.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputBackgroundDirective]'
 })
 export class InputBackgroundDirectiveDirective {
+  @Input() appInputBackgroundDirective: string = 'white';
+  @Input() textColor: string = 'black';
 
-  constructor() { }
+  @HostBinding('style.background-color') backgroundColor = 'transparent';
+  @HostBinding('style.color') color: string = '';
 
+  @HostListener('focus') onFocus() {
+    this.backgroundColor = this.appInputBackgroundDirective;
+    this.color = this.textColor;
+  }
+
+  @HostListener('blur') onblur() {
+    this.backgroundColor ='white';
+    this.color = 'black';
+  }
+
+  @HostListener('keyup') onKeyUp() {
+    this.color = this.textColor;
+  }
 }
