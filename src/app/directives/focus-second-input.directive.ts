b5d7c93baeb3d44335bd7fe7d09c31b4077e7171
thiago_osim <thiago_osim@hotmail.com>
2026-01-19 20:08:21 -0300

    Criado o elemento div referenciando o appFocusSecondInput

--- src/app/directives/focus-second-input.directive.ts
+++ src/app/directives/focus-second-input.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appFocusSecondInput]'
 })
 export class FocusSecondInputDirective {
-
-  constructor() { }
-
+  @HostBinding('style.backgroundColor') bgColor = 'blue';
+  @HostBinding('style.color') color = 'white';
+  @HostBinding('style.MarginTop.px') marginTop = '2';
+  @HostBinding('textContent') text = 'Sou uma div';
 }
