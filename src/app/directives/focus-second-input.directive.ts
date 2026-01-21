192f53659d11479c8290a0edf557d894bd295c9a
thiago_osim <thiago_osim@hotmail.com>
2026-01-21 15:55:46 -0300

    Obtido referência dos elementos HTML através do ElementRef e realizado interação através do event focus

--- src/app/directives/focus-second-input.directive.ts
+++ src/app/directives/focus-second-input.directive.ts
@@ -1,11 +1,24 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Directive({
   selector: '[appFocusSecondInput]'
 })
-export class FocusSecondInputDirective {
-  @HostBinding('style.backgroundColor') bgColor = 'blue';
-  @HostBinding('style.color') color = 'white';
-  @HostBinding('style.MarginTop.px') marginTop = '2';
-  @HostBinding('textContent') text = 'Sou uma div';
+export class FocusSecondInputDirective implements AfterViewInit {
+  // @HostBinding('style.backgroundColor') bgColor = 'blue';
+  // @HostBinding('style.color') color = 'white';
+  // @HostBinding('style.MarginTop.px') marginTop = '2';
+  // @HostBinding('textContent') text = 'Sou uma div';
+
+  constructor(private readonly _elRef: ElementRef) { }
+
+  ngAfterViewInit(): void {
+    console.log('elRef', this._elRef);
+
+    const inputList = this._elRef.nativeElement.querySelectorAll('input') as HTMLInputElement[];
+    console.log(inputList);
+
+    if(inputList.length > 1) {
+      inputList[1].focus();
+    }
+  }
 }
