66ad73a2a0d3c047c0ff438624942dfbb66d5c3e
thiago_osim <thiago_osim@hotmail.com>
2026-01-22 20:03:25 -0300

    Utilizado renderer2 para evitar a vulnerabilidade de XSS

--- src/app/xss/xss.component.ts
+++ src/app/xss/xss.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-xss',
@@ -6,7 +6,10 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
   styleUrl: './xss.component.scss'
 })
 export class XssComponent implements AfterViewInit {
-  constructor(private readonly _elRef: ElementRef) {}
+  constructor(
+    private readonly _elRef: ElementRef,
+    private readonly _renderer2: Renderer2
+  ) {}
 
   ngAfterViewInit(): void {
 
@@ -17,5 +20,14 @@ export class XssComponent implements AfterViewInit {
     // div.textContent = value;
     div.innerHTML = value;
     this._elRef.nativeElement.appendChild(div);
+  }
+
+  copyValueCorrect(value: string) {
+    const divEl = this._renderer2.createElement('div') as HTMLDivElement;
+    const text = this._renderer2.createText(value);
+    this._renderer2.appendChild(divEl, text);
+    this._renderer2.setStyle(divEl, 'backgroundColor', 'red');
+    this._renderer2.setStyle(divEl, 'color', 'white');
+    this._renderer2.appendChild(this._elRef.nativeElement, divEl);
   }
 }
