c9198b60c18df6f54df1ae3102293cd8115e10a6
thiago_osim <thiago_osim@hotmail.com>
2026-01-22 19:47:12 -0300

    Criado estrutura HTML e lógica do componente xss

--- src/app/xss/xss.component.ts
+++ src/app/xss/xss.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-xss',
   templateUrl: './xss.component.html',
   styleUrl: './xss.component.scss'
 })
-export class XssComponent {
+export class XssComponent implements AfterViewInit {
+  constructor(private readonly _elRef: ElementRef) {}
 
+  ngAfterViewInit(): void {
+
+  }
+
+  copyValue(value: string) {
+    const div = document.createElement('div');
+    // div.textContent = value;
+    div.innerHTML = value;
+    this._elRef.nativeElement.appendChild(div);
+  }
 }
