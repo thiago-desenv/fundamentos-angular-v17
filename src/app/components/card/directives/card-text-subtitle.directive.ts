b6c2caff1784ec7f5131f4c00a569bc7f8c98e28
thiago_osim <thiago_osim@hotmail.com>
2026-02-23 23:09:06 -0300

    Adicionado multiplos seletores no card-text-subtitle

--- src/app/components/card/directives/card-text-subtitle.directive.ts
+++ src/app/components/card/directives/card-text-subtitle.directive.ts
@@ -1,7 +1,7 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: 'app-card-text-subtitle',
+  selector: 'app-card-text-subtitle, [appCardTextSubtitle]',
   host: { 'class': 'ca-u-card-text-subtitle' }
 })
 export class CardTextSubtitleDirective {}
