c867226d0045ba3889f603da78c1aba335309323
thiago_osim <thiago_osim@hotmail.com>
2026-02-23 23:12:06 -0300

    Adicionado multiplos seletores no card-text-description

--- src/app/components/card/directives/card-text-description.directive.ts
+++ src/app/components/card/directives/card-text-description.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: 'app-card-text-description',
+  selector: 'app-card-text-description, [appCardTextDescription]',
   host: { 'class': 'ca-u-card-text-description' }
 })
 export class CardTextDescriptionDirective {
