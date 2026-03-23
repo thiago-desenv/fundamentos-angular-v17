f6d4feea149638cd58b0cb52b4a6e8e5d1485f5e
thiago_osim <thiago_osim@hotmail.com>
2026-03-22 22:24:38 -0300

    Verificado que o conteúdo do content carregado pelo ContentChild só está disponível para uso no ngAfterContentInit ou ngAfterViewInit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,20 @@
-import { Component, ContentChild, HostBinding, Input, OnInit, TemplateRef } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, HostBinding, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit, AfterContentInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit', this.content);
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit', this.content);
+  }
+  ngOnInit(): void {
+    console.log('OnInit', this.content);
+  }
   @ContentChild('meuTemplate') content!: TemplateRef<any>;
 
   // condition: boolean = false;
