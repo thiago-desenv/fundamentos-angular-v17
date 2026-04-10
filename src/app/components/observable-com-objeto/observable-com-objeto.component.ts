278755deeb75c6536cd376ff2ba02bf1d92702e4
thiago_osim <thiago_osim@hotmail.com>
2026-04-09 21:24:04 -0300

    Criado botões para captura captura de valores com asyncpipe

--- src/app/components/observable-com-objeto/observable-com-objeto.component.ts
+++ src/app/components/observable-com-objeto/observable-com-objeto.component.ts
@@ -1,4 +1,4 @@
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { IUserResponse } from '../../interfaces/user-response';
 import { UsersService } from './../../services/users.service';
 import { AfterContentInit, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
@@ -12,9 +12,13 @@ export class ObservableComObjetoComponent implements OnInit, OnDestroy {
   user: IUserResponse = {} as IUserResponse;
   userSubs: Subscription | undefined;
 
+  user$!: Observable<IUserResponse>;
+
   constructor(private readonly _userService: UsersService) { }
 
   ngOnInit() {
+  this.user$ = this._userService.getUserById(3);
+
     this.userSubs = this._userService.getUserById(1).subscribe((userResponse) => {
       this.user = userResponse;
     });
@@ -22,5 +26,9 @@ export class ObservableComObjetoComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.userSubs && this.userSubs?.unsubscribe();
+  }
+
+  getUserById(id: number) {
+    this.user$ = this._userService.getUserById(id);
   }
 }
